e6ba5ecebbcb30e26766d9172466fdd0336f806f
scp3125 <scp3125@qq.com>
2024-02-01 19:07:10 +0800

    增加指标描述和链接

--- components/description.tsx
+++ components/description.tsx
@@ -1,10 +1,21 @@
-import { Descriptions, DescriptionsProps } from "antd";
+import { Descriptions, DescriptionsProps, Tooltip } from "antd";
+import Link from "next/link";
 import * as dfd from "danfojs";
 import * as stats from "simple-statistics";
 
 interface DescriptionProps {
   t: (key: string) => string;
   datas: number[];
+}
+
+function getLabel(t: (key: string) => string, key: string) {
+  return (
+    <Tooltip title={t(key + ".desc")}>
+      <Link href={t(key + ".url")} target="_blank">
+        {t(key + ".name")}
+      </Link>
+    </Tooltip>
+  );
 }
 
 export function Description({ t, datas }: DescriptionProps) {
@@ -17,103 +28,103 @@ export function Description({ t, datas }: DescriptionProps) {
   const items: DescriptionsProps["items"] = [
     {
       key: "count",
-      label: t("count"),
+      label: getLabel(t, "count"),
       children: datas.length.toString(),
     },
     {
       key: "sum",
-      label: t("sum"),
+      label: getLabel(t, "sum"),
       children: stats.sum(datas).toString(),
     },
     // {
     //   key: "product",
-    //   label: t("product"),
+    //   label: getLabel(t, "product"),
     //   children: stats.product(datas).toString,
     // },
     {
       key: "max",
-      label: t("max"),
+      label: getLabel(t, "max"),
       children: stats.max(datas).toString(),
     },
     {
       key: "min",
-      label: t("min"),
+      label: getLabel(t, "min"),
       children: stats.min(datas).toString(),
     },
     {
       key: "mean",
-      label: t("mean"),
+      label: getLabel(t, "mean"),
       children: stats.mean(datas).toFixed(2),
     },
     {
       key: "harmonic_mean",
-      label: t("harmonic_mean"),
+      label: getLabel(t, "harmonic_mean"),
       children: harmonicMean(t, datas),
     },
     {
       key: "median ",
-      label: t("median"),
+      label: getLabel(t, "median"),
       children: stats.median(datas).toString(),
     },
     {
       key: "median_absolute_deviation",
-      label: t("median_absolute_deviation"),
+      label: getLabel(t, "median_absolute_deviation"),
       children: stats.medianAbsoluteDeviation(datas).toFixed(2),
     },
     {
       key: "mode",
-      label: t("mode"),
+      label: getLabel(t, "mode"),
       children: s.mode().toString(),
     },
     {
       key: "range",
-      label: t("range"),
+      label: getLabel(t, "range"),
       children: (stats.max(datas) - stats.min(datas)).toFixed(2),
     },
     {
       key: "interquartile_range",
-      label: t("interquartile_range"),
+      label: getLabel(t, "interquartile_range"),
       children: stats.interquartileRange(datas).toFixed(2),
     },
 
     // {
     //   key: "geometric_mean",
-    //   label: t("geometric_mean"),
+    //   label: getLabel(t, "geometric_mean"),
     //   children: stats.geometricMean(datas).toString(),
     // },
     {
       key: "root_mean_square",
-      label: t("root_mean_square"),
+      label: getLabel(t, "root_mean_square"),
       children: stats.rootMeanSquare(datas).toFixed(2),
     },
     {
       key: "standard_deviation",
-      label: t("standard_deviation"),
+      label: getLabel(t, "standard_deviation"),
       children: stats.standardDeviation(datas).toFixed(2),
     },
     {
       key: "sample_standard_deviation",
-      label: t("sample_standard_deviation"),
+      label: getLabel(t, "sample_standard_deviation"),
       children: stats.sampleStandardDeviation(datas).toFixed(2),
     },
     {
       key: "variance",
-      label: t("variance"),
+      label: getLabel(t, "variance"),
       children: stats.variance(datas).toFixed(2),
     },
     {
       key: "sample_variance",
-      label: t("sample_variance"),
+      label: getLabel(t, "sample_variance"),
       children: stats.sampleVariance(datas).toFixed(2),
     },
     {
       key: "sample_skewness",
-      label: t("sample_skewness"),
+      label: getLabel(t, "sample_skewness"),
       children: stats.sampleSkewness(datas).toFixed(2),
     },
     {
       key: "sample_kurtosis",
-      label: t("sample_kurtosis"),
+      label: getLabel(t, "sample_kurtosis"),
       children: stats.sampleKurtosis(datas).toFixed(2),
     },
   ];
