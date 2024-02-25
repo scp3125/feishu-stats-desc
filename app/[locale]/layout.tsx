22a4525acce39a8007b4e51baaf1f4bcf672c4ec
scp3125 <scp3125@qq.com>
2024-02-25 13:54:05 +0800

    Optimize layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,6 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import { locales } from "../../i18n";
 import { unstable_setRequestLocale } from "next-intl/server";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "feishu-pdf",
-  description: "feishu-pdf",
-};
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -25,12 +16,8 @@ export default function RootLayout({
   unstable_setRequestLocale(locale);
   const messages = useMessages();
   return (
-    <html lang={locale}>
-      <body className={inter.className}>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider messages={messages}>
+      {children}
+    </NextIntlClientProvider>
   );
 }
